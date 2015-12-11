0ec563dc764cabbd1e4ef9381bfb9176d2848042
perfect28 <18292088145@163.com>
2015-12-11 13:29:45 +0800

    add db and its relevant model

---
+++ app/src/main/java/com/example/kkpanda/coolweather/model/Province.java
@@ -0,0 +1,34 @@
+package com.example.kkpanda.coolweather.model;
+
+/**
+ * Created by kkpanda on 2015/12/11.
+ */
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
